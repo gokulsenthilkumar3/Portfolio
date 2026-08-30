eef89405dad14f217f692c8c440c27b8f5ddbd8a
Gokul Senthilkumar <gokulsenthilkumar3@gmail.com>
2026-08-30 17:49:22 +0530

    Add Sites-compatible production build

---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import { sites } from "@openai/sites-vite-plugin"
+import vinext from "vinext"
+import { defineConfig } from "vite"
+
+export default defineConfig({
+  plugins: [vinext(), sites()],
+})
